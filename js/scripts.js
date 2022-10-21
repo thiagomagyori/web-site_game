25515fc74bf28111cbdcb495d514e96dfeef44fe
Thiago Magyori <thiagosilvamagyori@mail.com>
2022-10-20 22:10:47 -0300

    Todo layout foi ratado e deixado responsivo

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready  
+
+    (
+        function()
+
+{
+        var btmobile = $('.bt-menu');
+                  $(btmobile).on('click', function()
+        {
+            $('.menu-container ul').toggleClass('abrir-menu');
+ });
+   
+});
